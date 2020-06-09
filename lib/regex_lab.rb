bacc7447f8ba4f2fd072ddd681581e1785b66bb1
seanbalke <sean.balke@gmail.com>
2020-06-09 15:21:15 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    word.match?(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match?(/^[A-Z].*[?.!]$/)
 end
 
 def valid_phone_number?(phone)
-
+    # binding.pry
+    phone.match?(/\({0,1}\d{3}[\)\s-]?\d{3}[\s-]?\d{4}/)
 end
